8f8294fa0d33f1603c69df4c52abefaaf6749fd3
syedkashiftirmizi <79271879+syedkashiftirmizi@users.noreply.github.com>
2023-10-07 09:26:59 +0500

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const systemGenNum = Math.floor(Math.random() * 10);
+
+const userInput = await inquirer.prompt([
+	{
+		type: "number",
+		name: "userGuess",
+		message: "Enter Your Guess between 1 to 10",
+	},
+]);
+
+const userGuess = userInput;
+if (userGuess === systemGenNum) {
+	console.log("You ans Matched");
+	console.log(`${userInput.userGuess} userGuess = ${systemGenNum} systemGuess`);
+} else {
+	console.log("Try Again");
+	console.log(
+		`${userInput.userGuess} userGuess != ${systemGenNum} systemGuess`
+	);
+}
